d14d1a54971903f757a1c13dca2f3ce8ba9f9304
Angelo <111171031+Angelo-Eyama@users.noreply.github.com>
2025-02-17 13:27:50 +0100

    Archivo .env de ejemplo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <h1>React App</h1>
-      <button onClick={handleReadRoot} className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-95">Click me</button>
+      <button type="button" onClick={handleReadRoot} className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-95">Click me</button>
 
       <h2 className='bg-amber-600 w-1/4 p-2 m-4 text-center' >{message}</h2>
     </div>
