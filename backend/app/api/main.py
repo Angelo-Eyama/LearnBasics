9effe6275d378cdd581103039e94d079a07dac6f
Angelo <111171031+Angelo-Eyama@users.noreply.github.com>
2025-02-20 10:07:26 +0100

    Llamada basica del frontend

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from app.api.routers import users, submissions, problems, roles, coments, notifications, reports
-from app.api.routers import login, test_cases
+from app.api.routers import login, test_cases, code
 
 api_router = APIRouter()
 api_router.include_router(login.router)
@@ -12,3 +12,4 @@ api_router.include_router(coments.router)
 api_router.include_router(notifications.router)
 api_router.include_router(reports.router)
 api_router.include_router(test_cases.router)
+api_router.include_router(code.router)
