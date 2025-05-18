23a6c331d428395e7d4315c0fb032311e74d3274
Angelo <111171031+Angelo-Eyama@users.noreply.github.com>
2025-05-18 22:38:57 +0200

    Eliminados datos mockeados de problemas

--- frontend/src/pages/problems/problems-list.tsx
+++ frontend/src/pages/problems/problems-list.tsx
@@ -9,7 +9,7 @@ import { Label } from "@/components/ui/label"
 import { Badge } from "@/components/ui/badge"
 import { Textarea } from "@/components/ui/textarea"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Search, CheckCircle2, MoreHorizontal, Flag } from "lucide-react"
+import { Search, MoreHorizontal, Flag } from "lucide-react"
 import { FaStar } from "react-icons/fa6";
 import { parseServerString } from "@/utils/utils"
 import {
@@ -20,52 +20,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog"
 import { getProblems } from "@/client"
-import { useQuery, useQueryClient } from "@tanstack/react-query"
-// Mock problems data
-const problems = [
-    {
-        id: "1",
-        title: "Two Sum",
-        difficulty: "Facil",
-        tags: ["Arrays", "Hash Table"],
-        description:
-            "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
-        solved: true,
-    },
-    {
-        id: "2",
-        title: "Reverse Linked List",
-        difficulty: "Facil",
-        tags: ["Linked List", "Recursion"],
-        description: "Given the head of a singly linked list, reverse the list, and return the reversed list.",
-        solved: false,
-    },
-    {
-        id: "3",
-        title: "Binary Tree Level Order Traversal",
-        difficulty: "Normal",
-        tags: ["Tree", "BFS", "Binary Tree"],
-        description: "Given the root of a binary tree, return the level order traversal of its nodes' values.",
-        solved: true,
-    },
-    {
-        id: "4",
-        title: "Merge K Sorted Lists",
-        difficulty: "Dificil",
-        tags: ["Linked List", "Divide and Conquer", "Heap"],
-        description:
-            "You are given an array of k linked-lists lists, each linked-list is sorted in ascending order. Merge all the linked-lists into one sorted linked-list and return it.",
-        solved: false,
-    },
-    {
-        id: "5",
-        title: "LRU Cache",
-        difficulty: "Normal",
-        tags: ["Hash Table", "Linked List", "Design"],
-        description: "Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.",
-        solved: false,
-    },
-]
+import { useQuery } from "@tanstack/react-query"
 
 export default function ProblemsPage() {
     const [searchQuery, setSearchQuery] = useState("")
